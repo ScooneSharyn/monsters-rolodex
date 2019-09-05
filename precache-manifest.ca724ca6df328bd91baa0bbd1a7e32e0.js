6edd7aa8d20cb9ef815012ae42d30b50414d26b9
siu <siu@siudeiMac.lan>
2019-09-05 23:28:17 +0800

    Updates

---
+++ precache-manifest.ca724ca6df328bd91baa0bbd1a7e32e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062b06c604f8f77e05ca873b1ea3db72",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "73d9d961b6ec8d02757a",
+    "url": "/monsters-rolodex/static/css/main.b4e83f4d.chunk.css"
+  },
+  {
+    "revision": "c8acdfd5ee399782ebc2",
+    "url": "/monsters-rolodex/static/js/2.3c6da1d1.chunk.js"
+  },
+  {
+    "revision": "73d9d961b6ec8d02757a",
+    "url": "/monsters-rolodex/static/js/main.ca026162.chunk.js"
+  },
+  {
+    "revision": "1e0ce0efd5dc4c3cccad",
+    "url": "/monsters-rolodex/static/js/runtime~main.ceda567a.js"
+  }
+]);
